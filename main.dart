21b61210d634fc2b926ac0a104e11d49d138045a
humg-it98 <59381002+humg-it98@users.noreply.github.com>
2020-09-18 17:27:54 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_covid_dashboard_ui/screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Corona App by NTN ',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
